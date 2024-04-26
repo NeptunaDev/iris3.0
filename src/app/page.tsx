3a4e20d1f704a6e60d7a7baf0d7494e17442cdbf
Juan Esteban <cardonaospinajuanesteban@gmail.com>
2024-04-26 10:17:05 -0500

    commit3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div>hola</div>
             By{" "}
             <Image
               src="/vercel.svg"
