b44f4dd405fdd49154509e823a965ba27d24a4a7
Juan Pablo Cortes <jpcortesg@eafit.edu.co>
2024-06-07 15:25:44 -0500

    Finished crud organization

--- src/app/api/organization/[id]/route.ts
+++ src/app/api/organization/[id]/route.ts
@@ -1,4 +1,8 @@
-import { updateOrganization } from "@/controllers/organization/organization.controller";
+import connectDB from "@/configuration/db";
+import {
+  deleteOrganization,
+  updateOrganization,
+} from "@/controllers/organization/organization.controller";
 import { verifyJwt } from "@/middlewares/jwt/verifyJwt.middleware";
 import { clientIsOwnerOfProject } from "@/middlewares/organization/organization.middleware";
 import validateSchema from "@/middlewares/schema/validate.middleware";
@@ -6,10 +10,14 @@ import { UpdateOrganizationSchema } from "@/schemas/organization/organization.sc
 import { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+connectDB();
+export async function PATCH(req: Request, { params }: Params) {
   try {
     const jwt = verifyJwt(req);
     if (jwt instanceof Error)
@@ -30,6 +38,33 @@ export async function PATCH(
     if (resValidateSchema) return resValidateSchema;
 
     const res = await updateOrganization(body, params.id);
+    return res;
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: error.message, status: 500 },
+        { status: 500 }
+      );
+    }
+  }
+}
+
+export async function DELETE(req: Request, { params }: Params) {
+  try {
+    const jwt = verifyJwt(req);
+    if (jwt instanceof Error)
+      return NextResponse.json(
+        { error: jwt.message, status: 401 },
+        { status: 401 }
+      );
+
+    const isOwner = await clientIsOwnerOfProject(jwt as JwtPayload, params.id);
+    if (isOwner instanceof Error)
+      return NextResponse.json(
+        { error: isOwner.message, status: 403 },
+        { status: 403 }
+      );
+    const res = await deleteOrganization(params.id);
     return res;
   } catch (error) {
     if (error instanceof Error) {
