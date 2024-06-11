6c564884497258ab63ce9732cc59803679df5457
Juan Pablo Cortes <jpcortesg@eafit.edu.co>
2024-06-11 17:04:00 -0500

    Upgrade view and unique device in view

--- src/app/api/view/route.ts
+++ src/app/api/view/route.ts
@@ -4,27 +4,37 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Personal
 import validateSchema from "@/middlewares/schema/validate.middleware";
-import { CreateViewSchema, GetViewSchema, UpdateViewSchema } from "@/schemas/view/view.schema";
-import { createView, getView, updateView } from "@/controllers/view/view.controller";
+import {
+  CreateViewSchema,
+  GetViewSchema,
+  UpdateViewSchema,
+} from "@/schemas/view/view.schema";
+import {
+  createView,
+  getView,
+  updateView,
+} from "@/controllers/view/view.controller";
 import { getQueries } from "@/utils/api/request/getQueries";
+import { verifyJwt } from "@/middlewares/jwt/verifyJwt.middleware";
+import { JwtPayload } from "jsonwebtoken";
 
 // Get view
 export async function GET(req: NextRequest) {
   try {
+    const jwt = verifyJwt(req);
+    if (jwt instanceof Error)
+      return NextResponse.json(
+        { error: jwt.message, status: 401 },
+        { status: 401 }
+      );
     // Get Data
     const queries = getQueries(req);
-    if (!("idAp" in queries)) {
-      return NextResponse.json(
-        { error: "idAp is required", status: 400 },
-        { status: 400 }
-      );
-    }
 
     // Validate schema
     const resValidateSchema = validateSchema(queries, GetViewSchema);
     if (resValidateSchema) return resValidateSchema;
 
-    const res = await getView(queries);
+    const res = await getView(queries, jwt as JwtPayload);
     if (res instanceof Error)
       return NextResponse.json(
         { error: res.message, status: 500 },
--- src/controllers/view/get.controller.ts
+++ src/controllers/view/get.controller.ts
@@ -4,11 +4,29 @@ import { NextResponse } from "next/server";
 
 // Personal
 import ViewModel from "@/models/View.model";
+import { JwtPayload } from "jsonwebtoken";
+import OrganizationModel from "@/models/Organization.model";
+import SiteModel from "@/models/Site.models";
+import APModel from "@/models/AP.model";
 
-const get = async (queries: any) => {
+const get = async (queries: any, jwt: JwtPayload) => {
   try {
     // Get data
-    const views = await ViewModel.find(queries);
+    const { id: idClient } = jwt;
+    const organizations = await OrganizationModel.find({ idClient });
+    const organizationsId = organizations.map(
+      (organization) => organization._id
+    );
+    const sites = await SiteModel.find({
+      idOrganization: { $in: organizationsId },
+    });
+    const aps = await APModel.find({
+      idSite: { $in: sites.map((site) => site._id) },
+    });
+    const views = await ViewModel.find({
+      idAp: { $in: aps.map((ap) => ap._id) },
+      ...queries,
+    });
 
     // Return response
     return NextResponse.json(
