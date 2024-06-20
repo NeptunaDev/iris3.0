f0aa72cf13289381b82753a5b7f5dd34624c1c62
Juan Pablo Cortes <jpcortesg@eafit.edu.co>
2024-06-20 11:00:02 -0500

    Solve problems in front

--- src/schemas/ap/update.schema.ts
+++ src/schemas/ap/update.schema.ts
@@ -3,7 +3,7 @@ import { CreateAp } from "./create.schema";
 
 export interface UpdateAp extends Omit<CreateAp, "idSite" | "mac"> {}
 
-export const Update = Joi.object<UpdateAp>({
+export const Update = Joi.object<CreateAp>({
   idSite: Joi.string().description("The id of the site associated with the AP"),
   mac: Joi.string().description("The mac address of the AP"),
   name: Joi.string().description("The name of the AP"),
